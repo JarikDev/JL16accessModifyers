d895db31f4e1dcc8f4deb5e937652eba126c980a
ivan4000 <sxln1@mail.ru>
2018-10-02 17:47:06 +0300

    JL16accessModifiers

---
+++ src/two/OtherClass.java
@@ -0,0 +1,9 @@
+package two;
+import one.TestClass;
+public class OtherClass extends TestClass{
+    void method(){
+        OtherClass otherClass=new  OtherClass();
+        System.out.println(otherClass.j);
+
+    }
+}
